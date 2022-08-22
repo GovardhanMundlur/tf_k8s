1b180466f44925ac9d9dc170cf5e8f05375a5d45
Mundlur Govardhan <mundlurgovardhan@IT001955.local>
2022-08-22 20:27:03 +0530

    added template for run script

---
+++ run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#tf infra initialize
+
+
+#Infra tf run
+
+
+##
+echo "infra setup is completed"
+###
+
+## validate for k8s config file
+
+##------##
+
+#tf k8s initialize
+echo "initialize tf for k8s objects"
+
+
+#k8s objects run
+
+echo "k8s objects setup is completed"
